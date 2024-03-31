6b7df07deb8555d9a367beb49c280db22de45ce9
rishikesh-revandikar <rishikeshrevandikar3110@gmail.com>
2024-03-31 12:22:02 +0530

    Preloader page

--- src/App.jsx
+++ src/App.jsx
@@ -11,26 +11,42 @@ import Profile from "./pages/Profile"
 import Inbox from "./pages/Inbox"
 import QRpage from "./pages/QRpage"
 import Bills from "./pages/Bills"
+import { useEffect, useState } from "react"
+import PreLoader from "./components/PreLoader"
 
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true)
 
+
+  useEffect(() => {
+    const load = () => {
+      setTimeout(() => {
+        setIsLoading(false)
+      }, 3000)
+    }
+
+    load()
+  }, [])
 
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route element={<PrivateRoutes><DashboardLayout /></PrivateRoutes>}>
-          <Route path="/order" element={<Order />} />
-          <Route path="/catalog" element={<Catalog />} />
-          <Route path="/inbox" element={<Inbox />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/bills" element={<Bills />} />
-        </Route>
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
+      {
+        isLoading ? <PreLoader /> :
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route element={<PrivateRoutes><DashboardLayout /></PrivateRoutes>}>
+              <Route path="/order" element={<Order />} />
+              <Route path="/catalog" element={<Catalog />} />
+              <Route path="/inbox" element={<Inbox />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/bills" element={<Bills />} />
+            </Route>
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+      }
     </>
   )
 }
